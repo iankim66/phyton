761c2acc090f7f94ef5a4a09883b3547751dae9a
Narii <madnarii@gmail.com>
2023-02-25 17:00:48 +0900

    p
    p

---
+++ 230128/list_pandas.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import os 
+## 함수 생성 
+## 매개변수 2개 patth, end_string 
+def list_df(path2, end_string):
+    # file_list 생성 
+    file_list = os.listdir(path2)
+    # 확장자가 end_string 과 같은 경우의 리스트만 추출. 
+    ## Case 1
+    file_list2 = [] 
+    for i in file_list:
+        if i.endswith (end_string):
+            file_list2.append(i)
+    ## case 2 
+    ## file_list3 = [ i for i in file_list if i.endswith(end_string)]
+    ## print(file_list2 == file_list3)
+# file_list2 의 리스트항목들을 데이터프레임화 하여, 
+# 하나의 데이터프레임으로 결합. 
+    total_df = pd.DataFrame()
+# file_list2 의 항목들을 가지고 반복문 실행 
+    for i in file_list2:
+    ## path 는 매개변수, i는 file_list2 항목의 값 
+        if end_string == '.csv':
+            df = pd.read_csv(path2 + i)
+        elif end_string == '.json':
+            df = pd.read_json(path2 + i)
+        elif (end_string =='.xlsx') | (end_string == '.xls'):
+            df = pd.read_excel(path2 + i)
+        else:
+           return "확장자가 잘못 입력되었습니다. " 
+        ## 불러온 데이터 프레임을 total_df 에 단순한 행 결합 
+        total_df = pd.concat([total_df, df] , axis = 0 , ignore_index=True)
+    return total_df # 합쳐진 데이터 프레임을 리턴
